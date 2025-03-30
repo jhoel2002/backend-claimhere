f86e0bb3560487dd24a6d1e788ed1df860cc8671
jhoel2002 <n00254208@upn.pe>
2025-03-30 11:29:46 -0500

    Inicializacion del proyecto

---
+++ src/main/java/com/application/claimhereweb/ClaimherewebApplication.java
@@ -0,0 +1,13 @@
+package com.application.claimhereweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimherewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimherewebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/claimhereweb/ClaimherewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.claimhereweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimherewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

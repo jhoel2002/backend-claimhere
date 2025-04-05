9d628d46915c6a4cd94be1ce0cb4d53b06b1560c
jhoel2002 <n00254208@upn.pe>
2025-04-05 16:42:17 +0000

    correccion de permisos scripts

--- scripts/run-all-scripts.sh
+++ scripts/run-all-scripts.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 # Ejecuta el script para crear el archivo .env
-chmod +x create-env.sh
 ./create-env.sh
 
 # Ejecuta el script para crear el hook post-checkout
-chmod +x create-post-checkout.sh
 ./create-post-checkout.sh
 
 # Ejecuta el script para crear el archivo launch.json
-chmod +x create-launch.sh
 ./create-launch.sh
 
 echo "Todos los scripts se han ejecutado exitosamente."

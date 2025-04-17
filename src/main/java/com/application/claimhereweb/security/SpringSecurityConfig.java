447974e79269167126d783aeaeb2bf7d6579e6ba
jhoel2002 <n00254208@upn.pe>
2025-04-17 07:47:17 +0000

    configuracion de permisos al swagger

--- src/main/java/com/application/claimhereweb/security/SpringSecurityConfig.java
+++ src/main/java/com/application/claimhereweb/security/SpringSecurityConfig.java
@@ -50,6 +50,7 @@ public class SpringSecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/api/users").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/users/getAllUserByAdminRol").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/webjars/**", "/swagger-resources/**").permitAll()
                 // TIENE QUE SER POR ORDEN  GET -> POST -> PUT -> DELETE
                 .anyRequest().authenticated())
                 .exceptionHandling(exceptionHandling -> exceptionHandling
